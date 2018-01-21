e5f767371fb64da8f97b46eda77c521262ddc146
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-01-21 16:19:13 +0530

    LEAPIN.C
    C program to check leap year or not

---
+++ LEAPIN.C
@@ -0,0 +1,15 @@
+#inclde<stdio.h>
+#include<conio.h>
+voidmain();
+{
+int year():
+clrscr();
+printf("\n enter the any year");
+scanf("%d" ,&anyyear);
+year("%4==0 && %100==0)
+printf("\n enterthe leap year");
+scanf("%d ,&leapyear);
+(year%400 ==0 && 
+printf("\n LEAP YEAR");
+getch();
+}
