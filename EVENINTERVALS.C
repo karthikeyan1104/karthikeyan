d0772f460f0cbadccc75922cda87a7f7bfeec225
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-01-31 16:56:48 +0530

    EVENINTERVALS.C
    a c program to display even numbers

---
+++ EVENINTERVALS.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void printEvenOdd(int cur, int limit);
+{
+int main()
+clrscr();
+int lowerLimit, upperLimit;
+// Input lower and upper limit from user
+printf("Enter lower limit: ");
+scanf("%d", &lowerLimit);
+printf("Enter upper limit: ");
+scanf("%d", &upperLimit);
+printf("Even/odd Numbers from %d to %d are: ", lowerLimit, upperLimit);
+printEvenOdd(lowerLimit, upperLimit); 
+getch();
+}
