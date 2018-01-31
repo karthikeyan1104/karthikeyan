043650a772537b4505ca849bea20047423f5cd02
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-01-31 15:59:54 +0530

    FACTORIAL.C
    a program to find the factorial of a number

---
+++ FACTORIAL.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main()
+clrscr();
+int i,f=1,num;
+printf("\n enter the number");
+scanf("%d" ,&num;
+for (i=1;i<=m,i++);
+f=f*i;
+printf("\n Factorial of %d" is :"%d",num,f);
+getch();
+}
+
+
