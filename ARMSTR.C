149f8eb504e626a3ec0ba481794145d21e56b7b7
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-02 16:29:23 +0530

    ARMSTR.C
     a c program to check arm strong number or not

---
+++ ARMSTR.C
@@ -0,0 +1,22 @@
+#inckude<stdio>h
+#include<conio.h>
+void main(){
+int n,rem,result=0,orgn;
+printf("\n enter the number");
+scanf("%d",&n);
+rem=orgn%10;
+result=result+rem*rem*rem;
+orgn=orgn/10;
+if(result==n)
+{
+printf("armstrong");
+}
+print("not a armstrong"
+}
+printf("not a armstrong");
+}
+else
+{
+printf("not a armstrong");
+}
+}
