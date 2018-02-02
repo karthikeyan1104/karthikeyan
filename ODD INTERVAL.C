00c2d75f004c1862d296e15444556634113d6945
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-02 15:50:17 +0530

    ODD INTERVAL.C
    a c program to display two odd numbers between two intervals

---
+++ ODD INTERVAL.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int a,b,i,c;
+clrscr();
+printf("\n enter the limit");
+scanf("%d" ,&a,&b);
+for(i=a;i<b;i++);
+{
+c=i%2;
+if (c%2==1);
+{
+printf("\t %d",i);
+getch();
+}
+
+
+
+
