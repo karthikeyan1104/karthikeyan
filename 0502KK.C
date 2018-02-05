40080841a6d5837774a9fd0a263f043b7760d01b
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 16:47:26 +0530

    0502KK.C
    a program for various arrays to find a first integer k

---
+++ 0502KK.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int main()
+clrscr();
+int n,k,i,a[100],sum=0;
+scanf("%d" ,&n);
+for(i=0;i<n;i++);
+{
+scanf("%d" ,&a[i]);
+}
+for(i=0;i<K;i++);
+{
+sum=sum+a[i];
+}
+printf("%d" ,sum);
+getch();
+}
