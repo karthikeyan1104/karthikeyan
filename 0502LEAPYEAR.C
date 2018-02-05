64dd45217f6d30cdde6d1f27756906f070c7ffe7
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 16:18:49 +0530

    0502LEAPYEAR.C
    A PROGRAM TO CHECK WHETHER IT IS LEAP YEAR OR NOT

---
+++ 0502LEAPYEAR.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+int main()
+{
+int x;
+clrscr();
+printf("enter the chatecter");
+scanf("%d",&x);
+if(x%4==0);
+{
+printf("leap year");
+}
+else
+{
+printf("not leap year");
+getch();
+}
+}
