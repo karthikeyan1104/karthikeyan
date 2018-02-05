8337464e3e5c704f1b143973c2d531165682b60e
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 16:11:50 +0530

    0502LARGENUMBER.C
    a program to find largest number among three numbers

---
+++ 0502LARGENUMBER.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+in t a,b,c;
+clrscr();
+if(a>b)&&(a>c)
+{
+printf("a is big");
+}
+else if(b>c)
+[
+printf(" b is big"):
+}
+else
+{
+{
+printf(" c is big"):
+getch();
+}
+
