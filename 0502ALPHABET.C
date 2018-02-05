f923e442f28ea07e9e521e2fc9f0e936768346e0
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 16:00:30 +0530

    0502ALPHABET.C
    a program to check a character is alphabet or not

---
+++ 0502ALPHABET.C
@@ -0,0 +1,14 @@
+#include,stdio.h>
+#include<conio.h>
+void main();
+{
+char ch;
+clrscr();
+printf("\n enter any character:");
+scanf("%c" ,&ch):
+if((ch>='a' && ch.='z') || (ch.='A' && ch<='Z''))
+printf(" the enterer character %c is an alphabet" , ch);
+else
+printf("\n the entered character %c is not an alphabet ",ch);
+getch();
+}
