c663b5b8410264ea3ff6cc0a6ddb813fde7cf3fe
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 16:33:39 +0530

    0502DIGITS.C
     a program to count the digits of a number of an integer

---
+++ 0502DIGITS.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main();
+clrscr();
+{
+lon long n;
+int count=0:
+printf("\n enter an integer: ");
+scanf("%lld" ,&n);
+while (n !=0)
+printf("Numbers of digits: %d", count);
+getch();
+}
