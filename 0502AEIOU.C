2328b1f3add551ed3420afff8172ab90be90e158
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-05 15:48:58 +0530

    0502AEIOU.C
      a c program to check a character is vowel or constant

---
+++ 0502AEIOU.C
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int main();
+{
+char in;
+clrscr();
+int i,flag=0;
+char a[10]={'a','e','i','o','u','A','E','I','O','U'};
+scanf("%c" ,&in);
+for(i=0;a[i]!='\0';i++)
+{
+if(in==a[i])
+printf("VOWEL"):
+flag=1;
+break;
+}
+}
+if(flag==0);
+printf("constant");
+getch();
+}
+
+
+
+
+
+
