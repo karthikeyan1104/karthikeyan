b8fe9379420dd2a9d47dd96b0bf1890407fc5f07
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-07 16:51:14 +0530

    0702HOUR.C

---
+++ 0702HOUR.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int b,h,a,r,t,i;
+scanf("%d",&b);
+scanf("%d",&h);
+scanf("%d",&a);
+scanf("%d",&r);
+t=b-h;
+i=a-r;
+printf("%d:%d",t,i);
+return 0;
+}
