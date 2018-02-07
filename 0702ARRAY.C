206d1b4e37dfec144acbdce2dbc8635cbf61a0ee
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-07 15:52:46 +0530

    0702ARRAY.C

---
+++ 0702ARRAY.C
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,e;
+printf("enter the num\n");
+scanf("%d",&a);
+printf("enter the numbers\n",a);
+scanf("%d%d%d",&b,&c,&e);
+if(e>c)
+printf("%d",e);
+else if(e>b)
+printf("%d",e);
+else if(c>b)
+printf("%d",c);
+else if(c>e)
+printf("%d",c);
+else if(b>c)
+printf("%d",b);
+else if(b>e)
+printf("%d",b);
+getch();
+}
+}
