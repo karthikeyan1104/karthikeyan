5bac210b35d660ec600c2a48284d47ac6e7d75db
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-07 16:19:50 +0530

    Update 0702KKARTHI.C

--- 0702KKARTHI.C
+++ 0702KKARTHI.C
@@ -1,36 +1,35 @@
 #include<stdio.h>
-int main()
+#include<conio.h>
+void main()
 {
-    int ar[MAXSIZE];
-    int i, j, n, t;
-    printf("Enter the value of num \n");
-    scanf("%d", &n);
-    printf("Enter the elements one by one \n");
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &ar[i]);
-    }
-    printf("Input array is \n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\n", ar[i]);
-    }
-    /*   Bubble sorting begins */
-    for (i = 0; i < n; i++)
-    {
-        for (j = 0; j < (n - i - 1); j++)
-        {
-            if (ar[j] > ar[j + 1])
-            {
-                t = ar[j];
-                ar[j] = ar[j + 1];
-                ar[j + 1] = t;            }
-        }
-    }
-    printf("Sorted array is...\n");
-    for (i = 0; i < n; i++)
-    {
-        printf("%d\n", ar[i]);
-    }
-    return 0;
+int ar[MAXSIZE];
+int i, j, n, t;
+printf("Enter the value of num \n");
+scanf("%d", &n);
+printf("Enter the elements one by one \n");
+for (i = 0; i <n i++) 
+scanf("%d", &ar[i]);
+}
+printf("Input array is \n");
+for (i = 0; i < n; i++)
+{
+printf("%d\n", ar[i]);
+}
+/* Bubble sorting begins */
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < (n - i - 1); j++)
+{
+if (ar[j] > ar[j + 1])
+{
+t = ar[j];
+ar[j] = ar[j + 1];
+ar[j + 1] = t;            }
+}
+}
+printf("Sorted array is...\n");
+for (i = 0; i < n; i++)
+{
+printf("%d\n", ar[i]);
+getch():
 }
