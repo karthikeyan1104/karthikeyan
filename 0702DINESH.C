ca24b4521609a9eb21eabdeb9b4542ce0a63dde3
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-07 16:10:20 +0530

    0702DINESH.C

---
+++ 0702DINESH.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main()
+{
+int arr[Min_SIZE];
+int ,h;
+printf("\n enter size of array:");
+scanf("%d" ,&N);
+printf("enter %d elements in the array :", N);
+for(I=0; i<N; i++)
+{
+scanf("%d" ,&arr[i]);
+}
+printf("enter %d elements in the array : ", N);
+for(I=0; i<N; i++)
+{
+scanf("%d", &arr[i]);
+}
+printf("\n Elemts in array are: ");
+for(i=0; i<N; i++)
+{
+printf("%d, ", arr[i]);
+getch();
+}
