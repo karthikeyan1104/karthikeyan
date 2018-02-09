4279a66c89d210158d7eed7af5e0a5b1cf45d0d3
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-09 16:41:42 +0530

    SATHYAIPS.C

---
+++ SATHYAIPS.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+int main()
+{
+char s[100];
+int len,count=0,i,ch;
+gets(s);
+len=strlen(s);
+for(i=0;i<len;i++)
+{
+if(s[i]==' ')
+count++; 
+}
+ch=len-count;
+printf("%d",ch);
+getch();
+}
