99599afda5408af6ab46f9feee95056e75cd3a3a
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-21 16:36:28 +0530

    55.C

---
+++ 55.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int mian();
+clrscr();
+char[s};
+printf("\n ente the string");
+scanf("%s ,&a);
+gets(s);
+if(!isdigit(s[0]));
+printf("True");
+else
+printf("False");
+getch();
+}
+}
