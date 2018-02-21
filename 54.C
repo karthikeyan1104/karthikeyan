606e675e6da50b23b595d3401d2c0c1d9e9d114c
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-21 16:22:01 +0530

    54.C

---
+++ 54.C
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main()
+{
+int nu,temp,sum=0;
+printf("\n enter the number");
+scanf("%d%d" ,&num);
+temp=num;
+while(num!=0)
+{
+temp=num%10;
+sum=sum+temp;
+num=num/10;
+{
+}
+{
+printf("\n%d" ,sum);getch*();
+
+
+
+
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int main() 
+{
+int num,temp,sum=0;
+printf("enter the number");
+scanf("%d",&num);
+t
