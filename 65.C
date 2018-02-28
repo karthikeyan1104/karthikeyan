7cd891966d2fa9c674114b22696f285859f5a3ef
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-02-28 16:39:52 +0530

    65.C

---
+++ 65.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+{
+void main();
+{
+int main(void) 
+{
+clrscr();
+	char str[50];
+	int i;
+	printf("enter the string:");
+	scanf("%[^\n]s",str);
+	if(printf("\n%s",str))
+	{
+		
+	}
+getch();
+}
