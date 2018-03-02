875677e817539029d9ae12463c47a5cdf73e6956
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-03-02 16:43:22 +0530

    70.C

---
+++ 70.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,a,b,c;
+clrscr();
+printf("\n enter the numbers:");
+scanf("%d" ,&n);
+for(i=1;i<=n;i++)
+{
+a=pow(2,i);
+if(a>n)
+{
+b=i;
+break;
+}
+}
+c=pow(2,b);
+printf("\n%d" ,&c);
+getch();
+}
