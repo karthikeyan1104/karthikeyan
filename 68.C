9be82228eb63d451c226c702229ae3f951af95a9
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-03-02 16:04:33 +0530

    68.C

---
+++ 68.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+{
+void main()
+{
+clrscr();
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("\n yes");
+}
+else
+{
+printf("\n no");
+}
+getch();
+}
