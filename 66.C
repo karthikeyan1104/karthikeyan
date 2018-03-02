39a616d4015939419acdb85e235174e5d1942484
karthikeyan1104 <35525657+karthikeyan1104@users.noreply.github.com>
2018-03-02 15:39:54 +0530

    66.C

---
+++ 66.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+{
+
+int main
+{
+clrscr();
+int a,i,count=0,n;
+scanf("%d %d",&a,&n);
+for(i=1;i<=n;i++)
+{
+if(a%i==0)
+count++;
+}
+if(count==2)
+printf("%d is the prime number",a);
+else
+printf("%d is not a prime number",a);
+getch();
+}
